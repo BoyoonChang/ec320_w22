7bce24439cc27abafbb98f3eb65eea69ab6e8adc
boyoon-c <bchang@uoregon.edu>
2022-01-30 14:03:48 -0800

    lab04 update

--- Labs/04-Introduction_lm/04-Exercise.R
+++ Labs/04-Introduction_lm/04-Exercise.R
@@ -1,20 +1,42 @@
-#' Read in the data and answer the following questions.
-#' Interpreting the regression results.
+#' Answer the following questions.
 #'
-state_data <- read_csv("03-Introduction_tidyverse_data.csv")
-state_data_2017 <- state_data %>%
-  filter(year == 2017)
+library(tidyverse)
 some_data <- tibble(
   X = 1:100 + rnorm(100,0,1),
-  Y = 0.5*X + rnorm(100,0,1)
+  Y = 0.5*X + rnorm(100,0,10)
 )
 
-#' 1. Run a linear regression.
-library(broom)
-lm(Y~X, some_data) %>% tidy()
+#' 1. Use `lm()` function to regress Y on X defined in some_data.
 
 
-#' 2. Write out codes to produce the same estimate for the $\beta_0$ and $\beta_1$.
-#' 3. Write out codes to produce the statistic for the $\beta_0$ and $\beta_1$.
 
-#' 4. Interpret the t-statistics and p-value.
+
+
+#' 2. Write out codes to produce $\beta_1$ using the formula.
+#' Make sure it produces the same estimate with that when using `lm()`.
+#'
+#' Formula for slope coefficient:
+#' $$\hat{\beta}_1 = \dfrac{\sum_{i=1}^n (Y_i - \bar{Y})(X_i - \bar{X})}{\sum_{i=1}^n  (X_i - \bar{X})^2}$$
+
+
+
+
+
+
+#' 3. Write out codes to produce $\beta_1$ using the formula.
+#' Make sure it produces the same estimate with that when using `lm()`.
+#'
+#' Formula for intercept coefficient:
+#' $$ \hat{\beta}_0 = \bar{Y} - \hat{\beta}_1 \bar{X} $$
+
+
+
+
+
+
+#' 4. Create a scatter plot about `some_data` that shows the relationship between $X$ and $Y$.
+#' Then add a layer using `geom_smooth()` to create a fitted line that uses `lm` method.
+
+
+
+
