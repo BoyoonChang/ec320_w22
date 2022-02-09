eb559407705892be3d5172636be14143f90cb5a0
boyoon-c <bchang@uoregon.edu>
2022-02-09 11:35:45 -0800

    fix typo

--- Labs/05-Introduction_lm_revisited/05-Exercise.R
+++ Labs/05-Introduction_lm_revisited/05-Exercise.R
@@ -2,13 +2,13 @@
 #' $$
 #' log(lifeExp_{i}) = \beta_0 + \beta_1log(gdpPercap_i) + u_i
 #' $$
-#' 
-#' Remove the # in front of the codes and fill in the blanks. 
+#'
+#' Remove the # in front of the codes and fill in the blanks.
 #' We are using `gapminder` dataset contained in the package `gapminder`. I've already attached relevant libraries.
 library(pacman)
 p_load(broom, tidyverse, gapminder)
 
-#' 1. Use `lm()` to estimate the above regression model store it as `reg2`. 
+#' 1. Use `lm()` to estimate the above regression model store it as `reg2`.
 # reg2 <- lm(______~_____, data = gapminder)
 
 
@@ -24,8 +24,8 @@ p_load(broom, tidyverse, gapminder)
 # t_stat =  (____-0)/______
 # t_stat
 
-#' 5. Using `qt()` function to find the 95% critical value for the test and compare it with the t-statistic you got from Q4. Recall that the degrees of freedom is calculated by the number of observations in the dataset - the number of parameters estimated. 
-#' 
+#' 5. Using `qt()` function to find the 95% critical value for the two-sided test and compare it with the t-statistic you got from Q4. Recall that the degrees of freedom is calculated by the number of observations in the dataset - the number of parameters estimated.
+#'
 # qt(p=0.975, df=_____)
 
 #' 6. Based on your $t$-stat, what is your conclusion? Do you reject/fail to reject the null hypothesis that GDP per capita has no effect on life expectancy?
