50cfe378b8b1a5e5e144ab285d5e676f053ee874
boyoon-c <bchang@uoregon.edu>
2022-02-27 22:54:18 -0800

    lecture14 lab update

---
+++ Labs/07-Log_transform_interaction/07-Exercise.R
@@ -0,0 +1,22 @@
+
+#' This is the recap of the lab material.
+#' We continue to use `EAWE01` dataset to do the regressions. Necessary packages are already loaded as follows:
+#'
+library(pacman)
+p_load(tidyverse, readr, huxtable, broom)
+
+#'
+#' 1. Read in the data and call it `wages`
+# wages <- read_csv('___________')
+
+#' 2. Regress `EARNINGS` on `FEMALE`.
+# lm(_____ ~ ______, data = wages)
+
+#' 3. Interpret the results especially the coefficient estimate of `FEMALE` in your own words.
+
+
+#' 4. Regress `log(EARNINGS)` on `S`, `EXP`, `FEMALE` and `S*FEMALE`
+
+#' 5. Interpret the results especially the coefficient estimate of `FEMALE:S` in your own words.
+
+
